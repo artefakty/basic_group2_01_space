29b627aa14ea74037129e53bad7da9f717c30c49
Piotr Kosek <piotr@kosek.com>
2018-03-17 11:41:24 +0100

    After 1st workshops

---
+++ Assets/ShipBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipBehaviour : MonoBehaviour {
+
+    public float Acceleration = 1.0f;
+    public float Deaccelleration = 1.0f;
+    public float MaxSpeed = 1.0f;
+    public float RotationSpeed = 45.0f;
+    public float AirResistance = 0.0f;
+
+    private float speed;
+
+	// Use this for initialization
+	void Start () {
+        speed = 0.0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+            speed += Acceleration * Time.deltaTime;
+        if (Input.GetKey(KeyCode.DownArrow))
+            speed -= Deaccelleration * Time.deltaTime;
+
+        speed = Mathf.Lerp(0.0f, speed, Mathf.Pow((1.0f - AirResistance), Time.deltaTime));
+
+        if (speed > MaxSpeed) speed = MaxSpeed;
+        if (speed < -MaxSpeed) speed = -MaxSpeed;
+
+        this.transform.Translate(0.0f, 0.0f, speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            this.transform.Rotate(0.0f, -RotationSpeed * speed / MaxSpeed * Time.deltaTime, 0.0f);
+        if (Input.GetKey(KeyCode.RightArrow))
+            this.transform.Rotate(0.0f, RotationSpeed * speed / MaxSpeed * Time.deltaTime, 0.0f);
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+<p align="center"><img src="http://artefakty.com.pl/logo.png" alt="Artefakty"></p>
+
+---
+
+# Welcome!
+
+Welcome to official *SKN Artefakty* github repository. These files contains all sources used during workshops, both offline and online. You are free to use them proved you follow license specified in the *LICENSE* file. Have fun and see you at our workshops!
+
+# This repository contains:
+
+When | Where | What
+:---: | :---: | :---:
+4:35PM - 5:50PM, March 15th, 2018|Faculty of Physics, University of Lodz (room A121)|First workshop of second group of basic introduction to Unity
+**4:35PM - 6:35PM, March 22th, 2018**|**Faculty of Physics, University of Lodz (room A121)**|**Second workshop of second group of basic introduction to Unity (planned)**
+
+# Contact information
+
+Just drop us a message at skn@artefakty.com.pl
